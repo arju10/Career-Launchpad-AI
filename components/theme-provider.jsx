6feba92428139fe8326f2de9eeb951fa1eff04b6
arju10 <arjuarju71@gmail.com>
2025-05-01 12:31:54 +0600

    Setup backend with neon postgresql

--- components/theme-provider.jsx
+++ components/theme-provider.jsx
@@ -6,3 +6,5 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 export function ThemeProvider({ children, ...props }) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
+
+
